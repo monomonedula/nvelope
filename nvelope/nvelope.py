ee7e95aa1fb09fc70cc5d21d12ae0d754183e276
monomonedula <valh@tuta.io>
2022-01-31 18:02:33 +0200

    some fixes

--- nvelope/nvelope.py
+++ nvelope/nvelope.py
@@ -153,7 +153,10 @@ class Obj(Compound):
     def schema(cls) -> Dict[str, JSON]:
         return {
             "type": "object",
-            **{name: item.schema() for name, item in cls._conversion.items()},
+            "properties": {
+                name: item.schema() for name, item in cls._conversion.items()
+            },
+            "required": [f.name for f in fields(cls) if not maybe_missing_field(f)],
         }
 
 
@@ -485,13 +488,11 @@ class Validated(Conversion[T]):
         return self._c.schema()
 
 
-datetime_iso_format_conv: Conversion[datetime.datetime] = Validated(
-    c=ConversionOf(
-        to_json=lambda v: v.isoformat(),
-        from_json=lambda s: datetime.datetime.fromisoformat(cast(str, s)),
-        schema={
-            "type": "string",
-            "pattern": r"^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$",
-        },
-    ),
+datetime_iso_format_conv: Conversion[datetime.datetime] = ConversionOf(
+    to_json=lambda v: v.isoformat(),
+    from_json=lambda s: datetime.datetime.fromisoformat(cast(str, s)),
+    schema={
+        "type": "string",
+        "pattern": r"^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$",
+    },
 )
---
+++ tests/test_schema.py
@@ -0,0 +1,183 @@
+import datetime
+from dataclasses import dataclass
+from typing import Optional
+
+import jsonschema.exceptions
+import pytest
+from jsonschema import validate
+
+from nvelope import (
+    int_conv,
+    Obj,
+    CompoundConv,
+    string_conv,
+    Arr,
+    OptionalConv,
+    bool_conv,
+    MaybeMissing,
+    ObjWithAliases,
+)
+
+
+def test_basic_schema():
+    @dataclass
+    class Bar(Obj):
+        _conversion = {"xyz": int_conv}
+
+        xyz: int
+
+    class ArrInner(Arr):
+        conversion = CompoundConv(Bar)
+
+    @dataclass
+    class Inner(Obj):
+        _conversion = {
+            "inner_field": string_conv,
+            "arr_field": CompoundConv(ArrInner),
+        }
+
+        inner_field: str
+        arr_field: ArrInner
+
+    @dataclass
+    class Dummy(Obj):
+        _conversion = {
+            "foo": CompoundConv(Inner),
+        }
+        foo: Inner
+
+    assert Dummy.schema() == {
+        "type": "object",
+        "properties": {
+            "foo": {
+                "type": "object",
+                "properties": {
+                    "inner_field": {"type": "string"},
+                    "arr_field": {
+                        "type": "array",
+                        "items": {
+                            "type": "object",
+                            "properties": {"xyz": {"type": "integer"}},
+                            "required": ["xyz"],
+                        },
+                    },
+                },
+                "required": [
+                    "inner_field",
+                    "arr_field",
+                ],
+            }
+        },
+        "required": ["foo"],
+    }
+
+    validate(
+        Dummy(Inner("hello there", ArrInner([Bar(543)]))).as_json(), Dummy.schema()
+    )
+
+
+def test_schema_optional():
+    @dataclass()
+    class Foo(Obj):
+        _conversion = {
+            "abc": OptionalConv(string_conv),
+        }
+
+        abc: Optional[datetime.datetime]
+
+    assert Foo.schema() == {
+        "type": "object",
+        "properties": {"abc": {"type": ["string", "null"]}},
+        "required": ["abc"],
+    }
+
+    validate({"abc": None}, Foo.schema())
+    validate({"abc": "hello"}, Foo.schema())
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        validate({}, Foo.schema())
+
+
+def test_schema_optional_obj():
+    @dataclass
+    class Inner(Obj):
+        _conversion = {
+            "abc": int_conv,
+            "efg": bool_conv,
+            "xyz": string_conv,
+        }
+        abc: int
+        efg: bool
+        xyz: str
+
+    @dataclass
+    class Foo(Obj):
+        _conversion = {
+            "opt": OptionalConv(CompoundConv(Inner)),
+        }
+
+        opt: Optional[Inner]
+
+    assert Foo.schema() == {
+        "type": "object",
+        "properties": {
+            "opt": {
+                "type": ["object", "null"],
+                "properties": {
+                    "abc": {"type": "integer"},
+                    "efg": {"type": "boolean"},
+                    "xyz": {"type": "string"},
+                },
+                "required": ["abc", "efg", "xyz"],
+            }
+        },
+        "required": ["opt"],
+    }
+
+    validate({"opt": None}, Foo.schema())
+    validate({"opt": {"abc": 1234, "efg": False, "xyz": "Hello"}}, Foo.schema())
+    with pytest.raises(jsonschema.exceptions.ValidationError):
+        validate({"opt": {"abc": 1234, "xyz": "Hello"}}, Foo.schema())
+
+
+def test_validation_maybe_missing():
+    @dataclass()
+    class Foo(Obj):
+        _conversion = {"xyz": string_conv, "abc": string_conv}
+        xyz: MaybeMissing[str]
+        abc: str
+
+    assert Foo.schema() == {
+        "type": "object",
+        "properties": {"xyz": {"type": "string"}, "abc": {"type": "string"}},
+        "required": ["abc"],
+    }
+
+
+def test_arr_schema():
+    class Foo(Arr):
+        conversion = string_conv
+
+    assert Foo.schema() == {
+        "type": "array",
+        "items": {"type": "string"},
+    }
+
+
+def test_obj_with_aliases_schema():
+    class Foo(ObjWithAliases):
+        _alias_to_actual = {
+            "def_": "def",
+        }
+        _conversion = {
+            "def_": string_conv,
+            "foo": int_conv,
+        }
+
+        def_: str
+        foo: int
+
+    assert Foo.schema() == {
+        "def": {"type": "string"},
+        "foo": {"type": "integer"},
+        "type": "object",
+    }
